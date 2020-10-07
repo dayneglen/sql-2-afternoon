371c4814cc5d165107e163f7365dcd438e199a53
dayneglen <dayneglen@gmail.com>
2020-10-07 17:34:49 -0600

    finished nested part

--- practice.sql
+++ practice.sql
@@ -1,3 +1,4 @@
+-- Joining Practice
 -- 1
 SELECT * FROM invoice i
 JOIN invoice_line il ON il.invoice_id = i.invoice_id
@@ -28,3 +29,50 @@ SELECT a.title, t.name FROM track t
 JOIN genre g ON t.genre_id = g.genre_id
 JOIN album a ON t.album_id = a.album_id
 WHERE g.name = 'Alternative & Punk';
+-- Black Diamond
+SELECT t.name, g.name, al.title, ar.name FROM playlist_track pt
+JOIN track t ON pt.track_id = t.track_id
+JOIN genre g ON t.genre_id = g.genre_id
+JOIN album al ON t.album_id = al.album_id
+JOIN artist ar ON al.artist_id = ar.artist_id
+WHERE pt.playlist_id = 1;
+
+-- Nested Queries
+-- 1
+SELECT * FROM invoice
+WHERE invoice_id IN (
+	SELECT invoice_id FROM invoice_line
+	WHERE unit_price > 0.99
+);
+-- 2
+SELECT * FROM playlist_track
+WHERE playlist_id IN (
+	SELECT playlist_id FROM playlist
+	WHERE name = 'Music'
+);
+-- 3
+SELECT name from track
+WHERE track_id IN (
+	SELECT track_id FROM playlist_track
+	WHERE playlist_id = 5
+);
+-- 4
+SELECT * from track
+WHERE genre_id IN (
+	SELECT genre_id FROM genre
+	WHERE name = 'Comedy'
+);
+-- 5
+SELECT * FROM track
+WHERE album_id IN (
+	SELECT album_id FROM album
+	WHERE title = 'Fireball'
+);
+-- 6
+SELECT * FROM track
+WHERE album_id IN (
+	SELECT album_id FROM album
+	WHERE artist_id IN (
+		SELECT artist_id FROM artist
+		WHERE name = 'Queen'
+));
